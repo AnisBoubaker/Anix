75137e1ccc6f6900c6e53eb0c0dea48c0a0203ee
Anis Boubaker <anis@boubaker.ca>
2019-05-07 20:37:42 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+	require_once("./config.php");
+	$RUNTIMECONF_pageTitle=_("CGC | Canadian Geoexchange Coalition | Geothermal");
+	$RUNTIMECONF_selected_menu=1;
+	include("./html_header.php");
+	//print_r($menu->menuTable);
+?>
+<div id='main_center'>
+	<h1>Latest News &amp; Announcements</h1>
+	<div class='main_section'>
+		<h2>CGC Global Quality GeoExchange Program</h2>
+		Since the release of the recent goverment incentive programs about energy efficiency and global
+		warming preservation, CGC made some clarifications about the the Global Quality GeoExchange
+		Program including the NRCan's ecoEnergy Retroprofit for Home Incentive.
+		<a class='readmore' href='./'>Read more</a>
+	</div>
+	<div class='main_section'>
+		<img src='./images/image_house.jpg' alt="CGC's Global Quality GeoExchange Program" />
+		<h2>CGC Global Quality GeoExchange Program</h2>
+		Since the release of the recent goverment incentive programs about energy efficiency and global
+		warming preservation, CGC made some clarifications about the the Global Quality GeoExchange
+		Program including the NRCan's ecoEnergy Retroprofit for Home Incentive.
+		<a class='readmore' href='./'>Read more</a>
+	</div>
+	<div class='main_section'>
+		<h2>CGC Global Quality GeoExchange Program</h2>
+		Since the release of the recent goverment incentive programs about energy efficiency and global
+		warming preservation, CGC made some clarifications about the the Global Quality GeoExchange
+		Program including the NRCan's ecoEnergy Retroprofit for Home Incentive.
+		<a class='readmore' href='./'>Read more</a>
+	</div>
+	<div class='main_section'>
+		<h2>CGC Global Quality GeoExchange Program</h2>
+		Since the release of the recent goverment incentive programs about energy efficiency and global
+		warming preservation, CGC made some clarifications about the the Global Quality GeoExchange
+		Program including the NRCan's ecoEnergy Retroprofit for Home Incentive.
+		<a class='readmore' href='./'>Read more</a>
+	</div>
+</div><!-- main_center -->
+<div id='main_right'>
+	<img id='wgein_button' src='./images/button_wgein.jpg' alt="<?php echo _("World Ground Energy Information Network"); ?>" />
+	<div class='box'>
+		<h1>Latest News</h1>
+		<div class='content'>
+			<h3>CGC's 2007 board elections</h3>
+			<img src='./images/news_image.jpg' alt="CGC's 2007 board elections" style='float:right;padding-right:6px' />
+			<span class='note'>2007-07-01</span><br />
+			LIoyd Kuczek (Manitoba Hydro) has been re-elected as the chairman of the CGC's board.
+			<a class='readmore' href='./'>Read more</a>
+		</div>
+		<div class='content'>
+			<h3>CGC Releases Accreditation Application Form for Residential Designers</h3>
+			<span class='note'>2007-07-01</span><br />
+			The Canadian GeoExchange™ Coalition (CGC) has released the application form for CGC Residential Design Accreditation today. This is the second of four accreditations for industry specialists.
+			<a class='readmore' href='./'>Read more</a>
+		</div>
+		<div class='close_box'></div>
+	</div><!-- box -->
+	<div class='box'>
+		<h1>Latest Publications</h1>
+		<div class='content'>
+			<h3>Cruise ship onboard heatpumps</h3>
+			<span class='note'>2007-06-11 - Market Studies</span>
+		</div>
+		<div class='content'>
+			<h3>Cruise ship onboard heatpumps</h3>
+			<span class='note'>2007-06-11 - Market Studies</span>
+		</div>
+		<div class='close_box'></div>
+	</div><!-- box -->
+</div><!-- main_right -->
+<?php
+	include("./html_footer.php");
+?>
